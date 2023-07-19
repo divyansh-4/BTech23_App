6e3cef19210666b7328d708982657ab1da9476ce
aayush017 <ayushmishra086@gmail.com>
2023-07-19 23:34:56 +0530

    sidebar

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,26 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'B.Tech Induction App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple),
+        appBarTheme: const AppBarTheme(
+          color: Colors.deepPurple,
+        ),
+        primaryColor: Colors.deepPurple,
+        hintColor: Colors.deepPurpleAccent,
+        primaryColorDark: Colors.deepPurple[700],
+        primaryColorLight: Colors.deepPurple[200],
+        canvasColor: Colors.white,
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.black),
+        ),
       ),
       home: const MyHomePage(title: 'IIIT-D Induction App'),
     );
@@ -22,7 +32,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -35,13 +45,82 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
+      drawer: buildSidebar(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          children: <Widget>[
+            Text(
+              'Welcome to IIIT-D Induction App!',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to the Profile Page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ProfilePage()),
+                );
+              },
+              child: Text('Go to Profile Page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Drawer buildSidebar() {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          Container(
+            height: 90.00, // Set the height of the drawer header
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+            ),
+            child: Center(
+              child: Text(
+                'IIITD',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Profile Page'),
+            onTap: () {
+              // Close the drawer and navigate to the Profile Page
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ProfilePage()),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile Page'),
+      ),
+      body: Center(
+        child: Text(
+          'This is the Profile Page',
+          style: TextStyle(fontSize: 24),
         ),
       ),
     );
